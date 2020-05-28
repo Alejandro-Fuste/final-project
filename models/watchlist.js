09a8d2463b37d99d37d55775638b65dd6c7dcd5f
Alejandro F <afuste7@gmail.com>
2020-05-28 19:57:48 -0400

    added watchlist schema and index file in models folder. Added routes folder and api-routes file. Added api-routes and model to db variable in server js file.

---
+++ models/watchlist.js
@@ -0,0 +1,22 @@
+// Require mongoose
+const mongoose = require('mongoose');
+
+// Create schema variable
+const Schema = mongoose.Schema;
+
+const watchlistSchema = new Schema({
+	name: {
+		type: String,
+		trim: true,
+		required: 'Enter the company name for the stock'
+	},
+	ticker: {
+		type: String,
+		trim: true,
+		required: 'Enter the ticker symbol for the company'
+	}
+});
+
+const Watchlist = mongoose.model('Watchlist', watchlistSchema);
+
+module.exports = Watchlist;
