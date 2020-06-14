38062fb33373f087df1045578ab3940a2050e568
yarocruz <yarocruz@gmail.com>
2020-06-14 12:23:07 -0400

    got most of the ungraded data is rendering

--- client/src/components/Dashboard/Dashboard.js
+++ client/src/components/Dashboard/Dashboard.js
@@ -141,7 +141,7 @@ export default function Dashboard() {
 	const classes = useStyles();
 	const [ open, setOpen ] = useState(false);
 	const [ searchStock, setSearchStock ] = useState();
-	const [ gradeData, setGradeData ] = useState();
+	//const [ gradeData, setGradeData ] = useState();
 
 	const searchRef = useRef();
 
@@ -166,22 +166,21 @@ export default function Dashboard() {
 				 let data = {
 				 	name: longName,
 				 	symbol,
-					 // 	year: res.data.earnings.financialsChart.yearly[3].date,
+					 // year: res.data.earnings.financialsChart.yearly[3].date,
 					 revenue: res.data.financialData.revenuePerShare.fmt,
 				 	grossProfit: res.data.financialData.grossMargins.fmt,
-				// 	operatingIncome: res.data.financialData.profitMargins.fmt,
+				 	operatingIncome: res.data.financialData.profitMargins.fmt,
 				// 	netIncome: res.data.earnings.financialsChart.yearly[3].earnings.fmt,
-				// 	operatingIncomeAndNetProfit: res.data.financialData.profitMargins.fmt,
 				// 	earningsPerShare: res.data.defaultKeyStatistics.trailingEps.fmt,
-				// 	totalCash: res.data.financialData.totalCash.fmt,
-				// 	totalDebit: res.data.financialData.totalDebt.fmt,
-				// 	debtToEquity: res.data.financialData.debtToEquity.fmt,
-				// 	currentRatio: res.data.financialData.currentRatio.fmt,
-				// 	quickRatio: res.data.financialData.quickRatio.fmt,
-				// 	returnOnAssets: res.data.financialData.returnOnAssets.fmt,
-				// 	returnOnEquity: res.data.financialData.returnOnEquity.fmt,
-				// 	operatingCashFlow: res.data.financialData.operatingCashflow.fmt,
-				// 	freeCashFlow: res.data.financialData.freeCashflow.fmt
+				 	totalCash: res.data.financialData.totalCash.fmt,
+				 	totalDebit: res.data.financialData.totalDebt.fmt,
+				 	debtToEquity: res.data.financialData.debtToEquity.fmt,
+				 	currentRatio: res.data.financialData.currentRatio.fmt,
+				 	quickRatio: res.data.financialData.quickRatio.fmt,
+				 	returnOnAssets: res.data.financialData.returnOnAssets.fmt,
+				 	returnOnEquity: res.data.financialData.returnOnEquity.fmt,
+				 	operatingCashFlow: res.data.financialData.operatingCashflow.fmt,
+				 	freeCashFlow: res.data.financialData.freeCashflow.fmt
 				 };
 
 				// let gData = [
@@ -208,8 +207,8 @@ export default function Dashboard() {
 				//
 				setSearchStock(data);
 				//setGradeData(gData);
-				console.log('Grade Data:');
-				console.log(gradeData);
+				// console.log('Grade Data:');
+				// console.log(gradeData);
 			})
 			.catch((err) => console.log(err));
 	}, []);
@@ -220,19 +219,20 @@ export default function Dashboard() {
 			ticker: searchRef.current.value.toUpperCase()
 		})
 			.then((res) => {
+				// Trying to destructure this out a bit.
+				const { longName, symbol } = res.data;
+				console.log(longName, symbol);
 				console.log(res.data);
-				setSearchStock(null);
-				setGradeData(null);
+
 				let data = {
-					name: res.data.shortName,
-					symbol: res.data.symbol,
-					year: res.data.earnings.financialsChart.yearly[3].date,
-					revenue: res.data.earnings.financialsChart.yearly[3].revenue.fmt,
+					name: longName,
+					symbol,
+					// year: res.data.earnings.financialsChart.yearly[3].date,
+					revenue: res.data.financialData.revenuePerShare.fmt,
 					grossProfit: res.data.financialData.grossMargins.fmt,
 					operatingIncome: res.data.financialData.profitMargins.fmt,
-					netIncome: res.data.earnings.financialsChart.yearly[3].earnings.fmt,
-					netIncomeProfitMargin: res.data.financialData.profitMargins.fmt,
-					earningsPerShare: res.data.defaultKeyStatistics.trailingEps.fmt,
+					// 	netIncome: res.data.earnings.financialsChart.yearly[3].earnings.fmt,
+					// 	earningsPerShare: res.data.defaultKeyStatistics.trailingEps.fmt,
 					totalCash: res.data.financialData.totalCash.fmt,
 					totalDebit: res.data.financialData.totalDebt.fmt,
 					debtToEquity: res.data.financialData.debtToEquity.fmt,
@@ -244,38 +244,38 @@ export default function Dashboard() {
 					freeCashFlow: res.data.financialData.freeCashflow.fmt
 				};
 
-				let gData = [
-					{ property: 'revenue', value: res.data.earnings.financialsChart.yearly[3].revenue.raw },
-					{ property: 'grossProfit', value: res.data.financialData.grossMargins.raw },
-					{ property: 'operatingIncome', value: res.data.financialData.operatingMargins.raw },
-					{ property: 'netIncome', value: res.data.financialData.profitMargins.raw },
-					{ property: 'earningsPerShare', value: res.data.defaultKeyStatistics.trailingEps.raw },
-					{ property: 'totalCash', value: res.data.financialData.totalCash.raw },
-					{ property: 'totalDebt', value: res.data.financialData.totalDebt.raw },
-					{ property: 'debtToEquity', value: res.data.financialData.debtToEquity.raw },
-					{ property: 'currentRatio', value: res.data.financialData.currentRatio.raw },
-					{ property: 'quickRatio', value: res.data.financialData.quickRatio.raw },
-					{ property: 'returnOnAssets', value: res.data.financialData.returnOnAssets.raw },
-					{ property: 'returnOnEquity', value: res.data.financialData.returnOnEquity.raw },
-					{ property: 'freeCashFlow', value: res.data.financialData.freeCashflow.raw }
-				];
-				gData.forEach((item, i) => {
-					item.letterGrade = GradingScale[item.property](item.value);
-				});
-
-				gData.push({ finalGrade: GradingScale.finalGrade(gData) });
+				// let gData = [
+				// 	{ property: 'revenue', value: res.data.earnings.financialsChart.yearly[3].revenue.raw },
+				// 	{ property: 'grossProfit', value: res.data.financialData.grossMargins.raw },
+				// 	{ property: 'operatingIncome', value: res.data.financialData.operatingMargins.raw },
+				// 	{ property: 'netIncome', value: res.data.financialData.profitMargins.raw },
+				// 	{ property: 'earningsPerShare', value: res.data.defaultKeyStatistics.trailingEps.raw },
+				// 	{ property: 'totalCash', value: res.data.financialData.totalCash.raw },
+				// 	{ property: 'totalDebt', value: res.data.financialData.totalDebt.raw },
+				// 	{ property: 'debtToEquity', value: res.data.financialData.debtToEquity.raw },
+				// 	{ property: 'currentRatio', value: res.data.financialData.currentRatio.raw },
+				// 	{ property: 'quickRatio', value: res.data.financialData.quickRatio.raw },
+				// 	{ property: 'returnOnAssets', value: res.data.financialData.returnOnAssets.raw },
+				// 	{ property: 'returnOnEquity', value: res.data.financialData.returnOnEquity.raw },
+				// 	{ property: 'freeCashFlow', value: res.data.financialData.freeCashflow.raw }
+				// ];
+				// gData.forEach((item, i) => {
+				// 	item.letterGrade = GradingScale[item.property](item.value);
+				// });
+				//
+				// gData.push({ finalGrade: GradingScale.finalGrade(gData) });
 
 				setSearchStock(data);
-				console.log(gData);
-				setGradeData(gData);
-				console.log('Grade Data:');
-				console.log(gradeData);
+				//console.log(gData);
+				//setGradeData(gData);
+				// console.log('Grade Data:');
+				// console.log(gradeData);
 			})
 			.catch((err) => console.log(err));
 	};
 
 	const addToWatchListHandler = () => {
-		API.saveToWatchlist(gradeData)
+		API.saveToWatchlist()
 			.then((res) => {
 				console.log(res.data);
 			})
@@ -389,8 +389,8 @@ export default function Dashboard() {
 										<Grid item xs={6}>
 											<Typography variant='h4'>Cash Flow Statement</Typography>
 											<Paper className={classes.paper}>
-												<h5>Operating Cash Flow{searchStock.operatingCashFlow}</h5>
-												<h5>Free Cash Flow{searchStock.freeCashFlow}</h5>
+												<h5>Operating Cash Flow: {searchStock.operatingCashFlow}</h5>
+												<h5>Free Cash Flow: {searchStock.freeCashFlow}</h5>
 											</Paper>
 										</Grid>
 									</React.Fragment>
