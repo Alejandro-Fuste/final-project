88bcb0357f4c7e5273c899cec508018f3509b98b
Alejandro F <afuste7@gmail.com>
2020-06-14 23:06:34 -0400

    added part 3 of mern stack login/auth app build

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,35 @@
-import React from "react";
+import React from 'react';
 import Dashboard from './components/Dashboard/Dashboard';
-import { BrowserRouter as Router} from "react-router-dom";
+import { BrowserRouter as Router } from 'react-router-dom';
+import jwt_decode from 'jwt-decode';
+import setAuthToken from './utils/setAuthToken';
+import { setCurrentUser, logoutUser } from './actions/authActions';
 
-function App () {
-    return (
-      <Router>
-        <Dashboard />
-      </Router>
-    );
+import { Provider } from 'react-redux';
+import store from './store';
+
+if (localStorage.jwtToken) {
+	const token = localStorage.jwtToken;
+	setAuthToken(token);
+
+	const decoded = jwt_decode(token);
+	store.dispatch(setCurrentUser(decoded));
+
+	const currentTime = Date.now() / 1000;
+	if (decoded.exp < currentTime) {
+		store.dispatch(logoutUser());
+		window.location.href = './login';
+	}
+}
+
+function App() {
+	return (
+		<Provider store={store}>
+			<Router>
+				<Dashboard />
+			</Router>
+		</Provider>
+	);
 }
 
 export default App;
