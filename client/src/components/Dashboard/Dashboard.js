a4efc566e0f8af5f6b20eced71107baa24634569
Alejandro F <afuste7@gmail.com>
2020-06-16 13:25:53 -0400

    removed console log from gData for each loop

--- client/src/components/Dashboard/Dashboard.js
+++ client/src/components/Dashboard/Dashboard.js
@@ -267,7 +267,6 @@ export default function Dashboard() {
 				];
 				gData.forEach((item, i) => {
 					item.letterGrade = GradingScale[item.property](item.value);
-					console.log(item);
 				});
 
 				gData.push({ finalGrade: GradingScale.finalGrade(gData) });
