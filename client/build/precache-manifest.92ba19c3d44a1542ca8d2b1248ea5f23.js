c2fa3c464e83afeb37de0a5b7e900d3de7abe505
Alejandro F <afuste7@gmail.com>
2020-06-17 17:40:38 -0400

    added build folder

---
+++ client/build/precache-manifest.92ba19c3d44a1542ca8d2b1248ea5f23.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "69a0ce9addd31a6a6cc0",
+    "url": "/static/css/main.c9d86742.chunk.css"
+  },
+  {
+    "revision": "69a0ce9addd31a6a6cc0",
+    "url": "/static/js/main.bdd577a2.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "998d6f046be2df9c1567",
+    "url": "/static/css/2.5cddac44.chunk.css"
+  },
+  {
+    "revision": "998d6f046be2df9c1567",
+    "url": "/static/js/2.dd0bb75d.chunk.js"
+  },
+  {
+    "revision": "9949df911887f78d2fe9e7247648ce93",
+    "url": "/static/media/logo 2.9949df91.svg"
+  },
+  {
+    "revision": "721a1535d7c7db12932085d578664846",
+    "url": "/static/media/login-bg.721a1535.png"
+  },
+  {
+    "revision": "ef0da0e72042dbbbfee9a5289bd2fa27",
+    "url": "/index.html"
+  }
+];
