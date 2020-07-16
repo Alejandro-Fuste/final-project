4c38a99c8cbae1b03f74120c550c534c4963b73a
Alejandro F <afuste7@gmail.com>
2020-07-16 14:35:28 -0400

    added dividend.js file to utils folder

---
+++ client/src/utils/dividend.js
@@ -0,0 +1,11 @@
+// The following function will be used to calculate the dividend amount the company will pay for one payment.
+
+// Variables for Principle Investment Amount, Stock Share Price, and Dividend Rate (Amount already divided by 12, 4, 2 if it's paid monthly, quarterly, or seminannually)
+
+let principleInvestment, sharePrice, dividendRate;
+
+// Function with dividend amount equation
+
+export default {
+	dividendPayment: function() {}
+};
