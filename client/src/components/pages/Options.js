2d057c091433445d53a5ff110848472c26f2826f
Alejandro F <afuste7@gmail.com>
2020-09-14 15:35:23 -0400

    added pseudo-code for paper trading in Options.js file

---
+++ client/src/components/pages/Options.js
@@ -0,0 +1,23 @@
+// This options page will be where paper trading will be located.
+
+// Paper trading will allow an investor to practice trading options
+
+// This will require the following:
+/*  - Allow user to input the following items:
+                1. ticker symbol: this is stock they want to trade
+                2. type of trade: buy, sell, straddle, strangle, iron condor, etc. 
+                3. contract(s): the amount of contracts
+                4. stock price: the price of the stock at the time of trade. This will be the initial price 
+                5. strike price:  
+                6. date: the date they want to end the paper trade
+
+    - Retrieve data of stock using the entered ticker symbol:
+                1. price: will need to get the price of the contract to calculate total cost (price * number of contracts)
+                2. stock price: will need to get final stock price to calculate gain/loss 
+ */
+
+// Calculate gain/loss of the trade
+
+// Store record of the trade in database
+
+// Display past trades in table and in chart
