8f7ec05e143d9f44891bc7ef8febc345e65d2237
Alejandro F <afuste7@gmail.com>
2020-09-18 13:20:11 -0400

    added code to module export to get option data

--- libs/optionChainAPI.js
+++ libs/optionChainAPI.js
@@ -3,7 +3,19 @@ const BASE_URI = 'https://option-chain.p.rapidapi.com/options/';
 
 const AuthHeader = () => {
 	return {
-		'x-rapidapi-host': process.env.API_HOST,
+		'x-rapidapi-host': process.env.API_HOST2,
 		'x-rapidapi-key': process.env.API_KEY
 	};
+};
+
+module.exports = {
+	getSummary: (query) => {
+		const { symbol = 'AAPL' } = query;
+
+		return axios.get(`${BASE_URI}${symbol}`, {
+			headers: {
+				...AuthHeader()
+			}
+		});
+	}
 };
